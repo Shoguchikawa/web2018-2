bc82433d0e54f6ef7847249782bde68faaf8cc9a
zm17429 <zm17429@josai.ac.jp>
2019-01-06 20:54:35 -0500

    message

---
+++ ex7-6.js
@@ -0,0 +1,5 @@
+function nazo4(){
+  if(document.getElementById('answer4').value=='夢'){
+    alert(' はずれ！');
+  }else if(document.getElementById('answer').value!=='夢')alert('あたり！');
+}
--- ex7-9.js
+++ ex7-9.js
@@ -2,5 +2,5 @@ function nazo3(){
   if(document.getElementById('answer2').value=='夢'){
     alert('あたり！');
   }else if(document.getElementById('answer2').value=='ゆめ'){alert('あたり！');
- }
- }
+ }else if(document.getElementById('answer2').value!=='夢')alert('はずれ！');
+}
