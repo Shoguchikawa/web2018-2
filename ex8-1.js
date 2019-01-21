96ba68600f22d156ea29779c4905dfb34667bfad
zm17429 <zm17429@josai.ac.jp>
2019-01-20 19:53:09 -0500

    message

---
+++ ex8-1.js
@@ -0,0 +1,6 @@
+function bloodtype(){
+  if(document.getElementById('a').value=='yes'){
+    alert('あなたの血液型はA型です');
+  }else if(document.getElementById('b').value=='yes'){alert('あなたの血液型はB型です');
+ }else if(document.getElementById('b').value!=='yes')alert('あなたの血液型はO型です');
+}
